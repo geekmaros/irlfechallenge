58175b6fea1ad5c3cae17cd6fb024973911e52ed
mustaphic <abdulrasaq.mustapha@demzanalytics.com>
2021-02-11 14:14:25 +0100

    added init files

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'irlfechallenge',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/tailwindcss'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    '@nuxtjs/apollo',
+  ],
+  apollo: {
+    clientConfigs: {
+      default: {
+        // process.env.IRL_ENDPOINT ||
+        // 'https://rickandmortyapi.com/graphql/'
+        // 'https://web.api.irl.com/.netlify/functions/graphql/'
+        httpEndpoint: 'https://web.api.irl.com/.netlify/functions/graphql/',
+        httpLinkOptions: {
+          fetchOptions: {
+            mode: 'cors', //Cors Needed for external Cross origins, need to allow headers from server
+          },
+          credentials: 'omit', //must be omit to support application/json content type
+        },
+      },
+    },
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
